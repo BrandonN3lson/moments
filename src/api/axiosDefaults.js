03b3c4806c688047b8b1cdce1fd0ab171fa20b36
Brandon Nelson <brandonnelson2@gmail.com>
2025-01-01 23:55:50 +0000

    add axios interceptors for refresh user access tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,9 @@ import axios from "axios"
 axios.defaults.baseURL = 'https://drf-api-app-2cb9957fb29d.herokuapp.com/'
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true
+
+
+// axios interceptors to allow the user to be logged in longer, basically renews the access token
+// if it has expired to prevent the user logging in every 5 min
+export const axiosReq = axios.create()
+export const axiosRes = axios.create()
